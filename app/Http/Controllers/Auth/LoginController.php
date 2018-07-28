5403b89df3a35c08aa74f17f25eb30a494407163
milad <m.rezai2724@gmail.com>
2018-07-28 14:55:02 +0430

    authorization passport login services

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,33 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function loginWithlaravelweb()
+    {
+        $query=http_build_query([
+           'client_id'=>3,
+           'redirect_uri'=>"http://localhost:7000/callback",
+            'response_type'=>'code',
+            'scope'=>''
+        ]);
+        return redirect("http://localhost:8000/oauth/authorize?".$query);
+    }
+
+    public function loginCallback(Request $request)
+    {
+        $http = new Client();
+
+        $response = $http->post('http://localhost:8000/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '3',
+                'client_secret' => 'x63YzQOcflvIA6XxOK2Y6XNtC0lmFDRjDikEROhT',
+                'redirect_uri' => 'http://localhost:7000/callback',
+                'code' => $request->code,
+            ],
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,13 @@
 
 
 
-Route::get('/','PanelController@index');
-Route::get('/post/{post}','PanelController@single')->name('post.single');
+//Route::get('/','PanelController@index');
+//Route::get('/post/{post}','PanelController@single')->name('post.single');
+
+
+Auth::routes();
+
+Route::get('login/laravelweb','Auth\LoginController@loginWithlaravelweb');
+Route::get('/callback','Auth\LoginController@loginCallback');
+
+Route::get('/home', 'HomeController@index')->name('home');
